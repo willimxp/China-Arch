7a848c08d5fd32f1822aa3be06085ebd422d5443
“willimxp” <42961@qq.com>
2022-10-29 22:29:07 +0800

    modified:   __init__.py

--- __init__.py
+++ __init__.py
@@ -11,7 +11,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-# test
+# test2 sda 
+# test2
 
 bl_info = {
     "name" : "PPtest",
