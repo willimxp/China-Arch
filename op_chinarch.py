b79eca4f1e97a1ff5c5dc1bb558b19617790e545
“willimxp” <42961@qq.com>
2022-11-01 21:56:06 +0800

    add

--- op_chinarch.py
+++ op_chinarch.py
@@ -2,6 +2,7 @@
 # 中式建筑插件 for blender
 
 
+from email.mime import base
 import bpy
 from bpy_extras.object_utils import AddObjectHelper, object_data_add
 
@@ -15,8 +16,12 @@ class CHINARCH_OT_build(bpy.types.Operator, AddObjectHelper):
     room_X : bpy.props.IntProperty(default=4)
     # 进深几间？
     room_Y : bpy.props.IntProperty(default=3)
+    # 间广（一般）
+    room_space : bpy.props.FloatProperty(default=3) 
     # 台基多高？
     base_Z : bpy.props.FloatProperty(default=1) 
+    # 台基多高？
+    piller_Z : bpy.props.FloatProperty(default=4) 
 
     def execute(self, context): 
         # 删除全部
@@ -33,6 +38,9 @@ class CHINARCH_OT_build(bpy.types.Operator, AddObjectHelper):
 
         # 创建地基
         print("PP: Build base")
+        base_border = 2
+        base_width = self.room_X * self.room_space + base_border
+        base_length = self.room_Y * self.room_space + base_border
         bpy.ops.mesh.primitive_cube_add(
             size=1.0, 
             calc_uvs=True, 
@@ -40,11 +48,33 @@ class CHINARCH_OT_build(bpy.types.Operator, AddObjectHelper):
             align='WORLD', 
             location=(0.0, 0.0, self.base_Z/-2), 
             rotation=(0.0, 0.0, 0.0), 
-            scale=(self.room_X, self.room_Y, self.base_Z))
+            scale=(base_width, base_length, self.base_Z))
         bpy.context.object.name = "台基"
         bpy.context.object.parent = root_obj
 
         # 创建柱网
+        print("PP: Build pillers")
+        offset_x = self.room_X * self.room_space / 2
+        offset_y = self.room_Y * self.room_space / 2
+        for x in range(self.room_X+1):
+            for y in range(self.room_Y+1):
+                loc_x = x * self.room_space - offset_x
+                loc_y = y * self.room_space - offset_y
+                bpy.ops.mesh.primitive_cylinder_add(
+                        vertices = 8, 
+                        radius = 0.25, 
+                        depth = self.piller_Z , 
+                        end_fill_type='NGON', 
+                        calc_uvs=True, 
+                        enter_editmode=False, 
+                        align='WORLD', 
+                        location=(loc_x, loc_y, self.piller_Z/2), 
+                        rotation=(0.0, 0.0, 0.0), 
+                        scale=(1,1,1)
+                    )
+                bpy.context.object.name = "立柱"
+                bpy.context.object.parent = root_obj
 
+        print("PP: finish")
 
         return {'FINISHED'}
