8721751508d557eb166c0ee78bd3b951eb65d703
“willimxp” <42961@qq.com>
2022-11-01 17:26:11 +0800

    modified:   __init__.py 	modified:   __pycache__/__init__.cpython-310.pyc 	modified:   __pycache__/pt_panel.cpython-310.pyc 	deleted:    auto_load.py 	deleted:    op_addmesh.py 	deleted:    op_hello.py

--- __init__.py
+++ __init__.py
@@ -11,8 +11,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 import bpy
-from . import op_addmesh
-from . import op_hello
 from . import pt_panel
 from . import op_chinarch
 
@@ -22,38 +20,18 @@ bl_info = {
     "description" : "Generate architecher in chinese style.",
     "blender" : (2, 80, 0),
     "version" : (0, 0, 1),
-    "location" : "View3D > N-Panel > China Arch",
-    "warning" : "",
+    "location" : "View3D > Properties > China Arch",
+    "tracker_url": "http://lucky3d.fr/auto-rig-pro/doc/bug_report.html",
+    "doc_url": "http://lucky3d.fr/auto-rig-pro/doc/",
     "category" : "Add Mesh"
 }
 
-# This allows you to right click on a button and link to documentation
-def add_object_manual_map():
-    url_manual_prefix = "https://docs.blender.org/manual/en/latest/"
-    url_manual_mapping = (
-        ("bpy.ops.mesh.add_object", "scene_layout/object/types.html"),
-    )
-    return url_manual_prefix, url_manual_mapping
-
-# from . import auto_load
-# auto_load.init()
-
 def register():
-    #auto_load.register()
-    bpy.utils.register_manual_map(add_object_manual_map)
     bpy.utils.register_class(pt_panel.CHINAARCH_PT_panel)
-    bpy.utils.register_class(op_hello.CHINAARCH_OT_hello)
-    bpy.utils.register_class(op_addmesh.CHINAARCH_OT_addobj)
     bpy.utils.register_class(op_chinarch.CHINARCH_OT_build)
-    
-    
 
 def unregister():
-    #auto_load.unregister()
-    bpy.utils.unregister_manual_map(add_object_manual_map)
     bpy.utils.unregister_class(pt_panel.CHINAARCH_PT_panel)
-    bpy.utils.unregister_class(op_hello.CHINAARCH_OT_hello)
-    bpy.utils.unregister_class(op_addmesh.CHINAARCH_OT_addobj)
     bpy.utils.unregister_class(op_chinarch.CHINARCH_OT_build)
 
 if __name__ == "__main__":
